6ddeb57d3e2f99725240300596b17794bf76c928
hhtangaa <hhtangaa@csl4wk13.win2k.cs.ust.hk>
2018-02-09 14:03:48 +0800

    task_1

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 
 public class HelloWorld {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		System.out.println("Tang Ho Hang");
+		System.out.println("20348848");
 	}
 }
